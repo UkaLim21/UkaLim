c881f9e3bf663655f51e32d57ecf8c1fe1ab7423
UkaLim21 <lucas.wag2105@gmail.com>
2025-09-10 14:55:47 +0000

    Nível Novato do Desafio de Lógica concluído

---
+++ TesteOperadoresRelacionaisSimples.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int idade = 17;
+
+if(idade >= 18){
+printf("Você é maior de Idade\n");
+printf("Dentro do IF\n");
+}
+
+printf("Fora do IF");
+
+
+
+
+
+return 0;
+}
---
+++ teste2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int numero1 = 25;
+int numero2 = 30;
+    
+if(numero1 > numero2){
+printf("O numero 1 é maior que o numero 2\n");
+}else {printf("O numero 1 é menor que o numero 2\n");
+
+
+}
+
+
+
+}
+
