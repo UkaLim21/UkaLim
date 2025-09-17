846ba2a84935efcd197eddbc1b24295009d81a84
UkaLim21 <lucas.wag2105@gmail.com>
2025-09-17 13:04:30 +0000

    Nível Aventureiro do Xadrez Concluído!!!

---
+++ LoopsAninhados.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+for (int i = 1; i <= 10; i++){
+    for (int j = 1; j <= 10; j++){
+        printf("O resultdo de %d vezes %d é igual a %d\n", i, j, i * j);
+    
+    }
+    printf("\n");
+}
+
+
+
+return 0;
+}
--- Trabalhos de Movimentação do Xadrez/Nível Aventureiro Xadez.c
+++ Trabalhos de Movimentação do Xadrez/Nível Aventureiro Xadez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-   
+    
     int i;
     int movimentosTorre = 5; 
     printf("Movimento da Torre:\n");
@@ -9,14 +9,17 @@ int main() {
         printf("Direita\n");
     }
 
+   
     int j = 1;
     int movimentosBispo = 5;
     printf("\nMovimento do Bispo:\n");
     while (j <= movimentosBispo) {
-        printf("Cima, Direita\n");
+        printf("Cima\n");
+        printf("Direita\n");
         j++;
     }
 
+   
     int k = 1;
     int movimentosRainha = 8;
     printf("\nMovimento da Rainha:\n");
@@ -24,6 +27,23 @@ int main() {
         printf("Esquerda\n");
         k++;
     } while (k <= movimentosRainha);
+
+    int passosBaixo = 2;
+    int passosEsquerda = 1;
+
+    printf("\nMovimento do Cavalo:\n");
+
+    
+    for (int m = 1; m <= passosBaixo; m++) {
+        printf("Baixo\n");
+    }
+
+   
+    int n = 1;
+    while (n <= passosEsquerda) {
+        printf("Esquerda\n");
+        n++;
+    }
 
     return 0;
 }
